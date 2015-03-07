558f364cbd4e6984882ad36eef8959206b285b36
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2015-03-07 01:25:46 +0100

    Fixes a bug in the options handling

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var ngAnnotate = require("ng-annotate");
 var loaderUtils = require("loader-utils");
 
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
 module.exports = function (content, map) {
     var callback = this.async();
     this.cacheable && this.cacheable();
 
-    var options = loaderUtils.parseQuery(this.query) || { add: true };
+    var query = loaderUtils.parseQuery(this.query);
+    var options = isEmpty(query) ? { add: true } : query;
     var result = ngAnnotate(content, options);
 
     if (result.errors) {
