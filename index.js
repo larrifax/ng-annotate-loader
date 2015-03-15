57ded0a035f36e8d6e7fa3c1871b14efc3c301d3
Kristian Tryggestad <kristian.tryggestad@gmail.com>
2015-03-15 16:05:07 +0100

    Fixes source maps when using e.g. ts-loader in front of this loader

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 var ngAnnotate = require("ng-annotate");
 var loaderUtils = require("loader-utils");
+var SourceMapConsumer = require("source-map").SourceMapConsumer;
+var SourceMapGenerator = require("source-map").SourceMapGenerator;
 
 function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
 
+function getOptions() {
+    var defaults = { add: true };
+    var query = loaderUtils.parseQuery(this.query);
+    var options = isEmpty(query) ? defaults : query;
+
+    if (this.sourceMap && options.sourcemap === undefined) {
+        options.sourcemap = { inline: false, inFile: this.resourcePath };
+    }
+
+    return options;
+}
+
 module.exports = function (content, map) {
+    var that = this;
     var callback = this.async();
     this.cacheable && this.cacheable();
 
-    var query = loaderUtils.parseQuery(this.query);
-    var options = isEmpty(query) ? { add: true } : query;
+    var options = getOptions.bind(this)();
     var result = ngAnnotate(content, options);
 
     if (result.errors) {
         result.errors.forEach(function(error) {
-            this.emitError(error);
+            that.emitError(error);
         });
     }
 
-    callback(null, result.src, map);
+    if (this.sourceMap) {
+        var originalSourcemap = new SourceMapConsumer(map);
+        var annotatedSourcemap = new SourceMapConsumer(result.map);
+        var resultingSourcemap = SourceMapGenerator.fromSourceMap(annotatedSourcemap);
+        resultingSourcemap.applySourceMap(originalSourcemap, this.resourcePath);
+    }
+
+    callback(null, result.src, resultingSourcemap === undefined ? map : resultingSourcemap.toJSON());
 }
